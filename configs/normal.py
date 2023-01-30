ee43c8da5aba5dd79613baf244ff2fb0ea072085
Dotdotdo <dkw088@gmail.com>
2023-01-30 12:13:56 +0100

    all models kind of work

--- configs/normal.py
+++ configs/normal.py
@@ -1,7 +1,7 @@
 # these are the tunable parameters to set the political landscape
 # used for presets in the base model
 n_agents = 100
-n_iterations = 100
+n_iterations = 5000
 char_distr = 'normal' # could also be 'uniform'
 until_eligible = 4
 characteristics_affected = {'active' : .4,
@@ -13,8 +13,9 @@ edges_per_step = 100
 prob_stimulus = 1 / 8
 prob_interaction = 1 / 8
 prob_move = 1 / 260
-prob_friend = 1 / 2
+# probability that a and c are linked in holme kim; delete later if possible
+prob_link = 1/2
 m_barabasi = 2
 fermi_alpha = 4
 fermi_b = 1.8
-network = "holme_kim"#"homophily" #'fully_connected'
+network = 'fully_connected' #"not_connected"#"holme_kim"#"homophily" #'fully_connected'
