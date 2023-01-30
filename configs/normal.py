048369659f64add82dec42fd980f830e065f4ef0
arendgeerlofs <arendgeerlofs@hotmail.nl>
2023-01-30 12:36:15 +0100

    save results over last 1000 iterations

--- configs/normal.py
+++ configs/normal.py
@@ -1,7 +1,7 @@
 # these are the tunable parameters to set the political landscape
 # used for presets in the base model
 n_agents = 100
-n_iterations = 100
+n_iterations = 5000
 char_distr = 'normal' # could also be 'uniform'
 until_eligible = 4
 characteristics_affected = {'active' : .4,
